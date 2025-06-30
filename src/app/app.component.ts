6a22014f97cfe8fd4e53e255cfff4381e5519f55
Rodrigo Gulka <rodrigogulka@gmail.com>
2025-06-30 11:13:59 -0300

    adição da lista de alunos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormularioComponent } from './formulario/formulario.component';
+import { ListalunosComponent } from './listalunos/listalunos.component'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [CommonModule, FormularioComponent, ListalunosComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'formsngtw';
-}
+export class AppComponent {}
