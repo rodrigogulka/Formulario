9202a4c6b413edb6d0b7e00f141f861752e13748
Rodrigo Gulka <rodrigogulka@gmail.com>
2025-06-30 08:42:12 -0300

    formulario criado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: '<router-outlet></router-outlet>',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  title = 'formsngtw';
 }
