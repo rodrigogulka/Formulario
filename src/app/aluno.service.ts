d680c6f3895fe29011b3dd1e1b221bbd12ba1d25
Rodrigo Gulka <rodrigogulka@gmail.com>
2025-07-04 15:35:35 -0300

    Alterações no Layout

--- src/app/aluno.service.ts
+++ src/app/aluno.service.ts
@@ -12,9 +12,9 @@ export class AlunoService {
     const alunosAtual = this.alunosSubject.value;
     this.alunosSubject.next([...alunosAtual, aluno]);
   }
-  removerAluno(index: number) {
+  removerAluno(index: number) { // Conexão com o listalunos.component.ts
   const alunosAtual = this.alunosSubject.value;
-  alunosAtual.splice(index, 1);
+  alunosAtual.splice(index, 1); // Remove aluno através do botão excluir - Incrementa valor 1+
   this.alunosSubject.next([...alunosAtual]);
 }
 
